35e3dc52db6f6d7187fb10fd6be18b58a2c728b3
stairwalker <darksha518@naver.com>
2017-08-05 20:12:52 +0900

    주식 분봉데이터 뽑기

--- PyStock.py
+++ PyStock.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QAxContainer import *
+from Kiwoom import *
 import os
 import glob
 
@@ -21,8 +22,12 @@ class MyWindow(QMainWindow):
         #self.setupUi(self)
 
         # 키움 API 접속
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom = Kiwoom()
+        self.kiwoom.comm_connect() 
 
+	    #키움 API 신호
+        #self.kiwoom.OnEventConnect.connect(self._event_connect)
+        #self.kiwoom.OnReceiveTrData.connect(self._receive_tr_data)
 
         # 타이틀 설정
         self.setWindowTitle("종목 코드")
@@ -33,14 +38,17 @@ class MyWindow(QMainWindow):
         self.timer1s.start(1000)
         self.timer1s.timeout.connect(self.timer1sec)
 
-        # 1초 타이머 설정
+        # 400ms 타이머 설정
         self.timer10ms = QTimer(self)
         self.timer10ms.start(400)
         self.timer10ms.timeout.connect(self.getdatatimer)
         self.getdataflag = 0
-        self.getdatacount = 0
+        self.getdatacount = -1
+
+        self.prvFilename = ""
 
         self.temp = 0
+
 
         self.setupUI()
 
@@ -77,58 +85,85 @@ class MyWindow(QMainWindow):
         text_time = current_time.toString("hh:mm:ss")
         time_msg = "현재시간: " + text_time
 
-        state = self.kiwoom.dynamicCall("GetConnectState()")
+        state = self.kiwoom.get_connect_state()
         if state == 1:
             state_msg = "서버 연결 성공"
             self.btn1.setDisabled(1)
             self.btn2.setDisabled(0)
             self.btn3.setDisabled(0)
-            #self.kiwoom.dynamicCall("CommConnect()")
         else:
             state_msg = "서버 미접속"
             self.btn1.setDisabled(0)
             self.btn2.setDisabled(1)
             self.btn3.setDisabled(1)
-
-
-
-
-
         self.statusbar.showMessage(state_msg + " | " + time_msg)
 
+
+
     def getdatatimer(self):
-        if self.getdataflag == 1:
-            if self.getdatacount < 1231:
-                ret = self.kiwoom.dynamicCall("SetInputValue(QString,QString)", "종목코드", self.codelist[self.getdatacount])
-                ret = self.kiwoom.dynamicCall("CommRqData(Qstring,Qstring,Qstring,QString)", "주식분봉차트조회","OPT10080","0",self.codelist[self.getdatacount])
-                self.getdatacount = self.getdatacount + 1
+        if self.kiwoom.dataReceived == 1:
+            filename = '.\\data\\' + "20170804" + '_' + self.codelist[self.getdatacount] + '.csv'
+            if filename == self.prvFilename :
+                with open(filename,'a') as sfile:
+                    i=0
+                    if self.kiwoom.dataunit != None :                    
+                        while i<len(self.kiwoom.dataunit) :
+                            for j in range(0,6):
+                                sfile.write(self.kiwoom.dataunit[i][j])
+                                if j != 5:
+                                    sfile.write(',')
+                                else:
+                                    sfile.write('\n')
+                            i=i+1                                    
+                        self.kiwoom.dataReceived = 0                
             else:
-                self.getdataflag = 0
-                self.getdatacount = 0
+                self.prvFilename = filename
+                with open(filename,'w') as sfile:
+                    i=0
+                    if self.kiwoom.dataunit != None :                    
+                        while i<len(self.kiwoom.dataunit) :
+                            for j in range(0,6):
+                                sfile.write(self.kiwoom.dataunit[i][j])
+                                if j != 5:
+                                    sfile.write(',')
+                                else:
+                                    sfile.write('\n')
+                            i=i+1                                    
+                        self.kiwoom.dataReceived = 0
 
+        else :            
+            if self.getdataflag == 1:
+                if self.kiwoom.remained_data == False : 
+                    self.getdatacount = self.getdatacount + 1
 
+                if self.getdatacount < len(self.codelist) :
+                    self.kiwoom.set_input_value("종목코드", self.codelist[self.getdatacount])
+                    self.kiwoom.set_input_value("시작일자", "20170804")
+                    self.kiwoom.set_input_value("종료일자", "20170804")
+                    self.kiwoom.set_input_value("수정주가구분", 0)
+                    if self.kiwoom.remained_data == True : 
+                        self.kiwoom.comm_rq_data("주식분봉차트조회", "opt10080", 2, "1001")                        
+                    else :
+                        self.kiwoom.comm_rq_data("주식분봉차트조회", "opt10080", 0, "1001")                
+                else:
+                    self.getdataflag = 0
+                    self.getdatacount = 0
 
-
-    def btn1_clicked(self):
-        #stockCode = "141000"
-        #ret = self.kiwoom.dynamicCall("SetInputValue(QString,QString)","종목코드",stockCode)
-        ret = self.kiwoom.dynamicCall("CommConnect()")
+    def btn1_clicked(self):        
+        self.kiwoom.comm_connect()
         print(ret)
         if ret == 0 :
-            self.listWidget.insertItem(1,"Connected")
+            self.listWidget.insertItem(1,"로그인 성공")
         else :
-            self.listWidget.insertItem(1, "Not connected")
-
-
+            self.listWidget.insertItem(1, "로그오프")
 
     def btn2_clicked(self):
-        codestring = self.kiwoom.dynamicCall("GetCodeListByMarket(QString)", ["0"])
+        codestring = self.kiwoom.get_kospi_code_list()
         self.codelist = codestring.split(";")
         self.getdataflag = 1
 
     def btn3_clicked(self):
         files = glob.glob('.\\data\\*.csv')
-        i=0
         while len(files) > i:
             with open(files[i], 'r') as sfile:    #파일 순차적으로 열기
                 fileline = sfile.readlines()
@@ -141,28 +176,11 @@ class MyWindow(QMainWindow):
                         oclock = int(featuredata[2][9:10])
 
                     with open(files[i], 'w') as sfile:
-                        i=i+1
+                        i=i+1   
 
-
-
-    def OnReceiveTrData(self, sScrNo, sRQName, sTRCode, sRecordName, sPreNext, nDataLength, sErrorCode, sMessage, sSPlmMsg):
-        #분봉차트 데이터 수신시
-        if sRQName == "주식분봉차트조회" :
-            revdata = self.kiwoom.dynamicCall("GetCommDataEx(QString,QString)", "OPT10080", "주식분봉차트조회")
-            self.temp = self.temp + 1
-            print(self.temp)
-            filename = '.\\data\\' + revdata[0][2][:8] + '_' + sScrNo + '.csv'
-            with open(filename,'w') as sfile:
-                i=0
-                while revdata[0][2][:8] == revdata[i][2][:8]:
-                    for j in range(0,6):
-                        sfile.write(revdata[i][j])
-                        if j != 5:
-                            sfile.write(',')
-                    sfile.write('\n')
-                    i=i+1
 
 if __name__ == "__main__":
+
     app = QApplication(sys.argv)
     myWindow = MyWindow()
     myWindow.show()
